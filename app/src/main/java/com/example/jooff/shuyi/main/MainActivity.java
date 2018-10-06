50ce100975e6794c3b88a5c706c611405b1004a3
lmovse <hmilyssy@gmail.com>
2018-10-06 13:54:03 +0800

    Update to version 2.5.2

--- app/src/main/java/com/example/jooff/shuyi/main/MainActivity.java
+++ app/src/main/java/com/example/jooff/shuyi/main/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
